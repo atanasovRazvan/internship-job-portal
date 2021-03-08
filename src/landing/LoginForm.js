c3e3e9b3eb8193459ad76f261d5eda66e54cab5a
Atanasov Mihai-Răzvan <62375099+atanasovRazvan@users.noreply.github.com>
2021-03-08 11:45:47 +0200

    Fixed linter error

--- src/landing/LoginForm.js
+++ src/landing/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import {
   Button, InputAdornment,
   FormControl, FormHelperText, Input, InputLabel, IconButton,
